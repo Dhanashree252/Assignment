d32da6a9a3e8402db897ffcc0ab6cb01a0ab679a
Dhanashree252 <mone.dhanashree25@gmail.com>
2022-02-17 16:23:16 +0530

    Easy Drag and Drop

---
+++ Easy_drag_drop/script.js
@@ -0,0 +1,45 @@
+const listItems = document.querySelectorAll(".list-item")
+const lists = document.querySelectorAll(".list")
+
+let draggedItem = null
+
+for(let i = 0; i<listItems.length; i++){
+    const item = listItems[i]
+    item.addEventListener("dragstart", function(e) {
+        // console.log("dragstart", e)
+        draggedItem = item
+        setTimeout(function(){
+            item.style.display = "none";
+        }, 0)
+    })
+
+    item.addEventListener("dragend", function(e) {
+        // console.log("dragend", e)
+        setTimeout(function(){
+            draggedItem.style.display = "block"
+            draggedItem = null
+        }, 0)
+    })
+}
+
+
+for(let j = 0; j < lists.length; j++){
+    const list = lists[j]
+
+    list.addEventListener("dragover", function(e){
+        e.preventDefault()
+    })
+    list.addEventListener("dragenter", function(e){
+        e.preventDefault()
+        this.style.backgroundColor = "rgba(0,0,0,0.2)"
+    })
+    list.addEventListener("dragleave", function(e){
+        this.style.backgroundColor = "rgba(0,0,0,0.1)"
+    })
+
+    list.addEventListener('drop', function(e){
+        // console.log("drop")
+        this.append(draggedItem)
+        this.style.backgroundColor = "rgba(0,0,0,0.1)"
+    })
+}
